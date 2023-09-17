f77ff63469d4a0492c8b6744f66bd04e59b36cbc
Moussa Aminata Gueye <kalamouu@gmail.com>
2023-09-17 20:01:09 +0000

    created a range input with the actual grid updating begun the color setter function

--- script.js
+++ script.js
@@ -1,36 +1,59 @@
-const sketchPad = document.querySelector('#sketchpad')
-const buttons = document.querySelectorAll('button')
-let SIZE = 8;
+const squareNumber = document.getElementById('squareNumber');
+const sketchPad = document.getElementById('sketchpad')
+const sliderValue = document.getElementById('sliderValue')
+const slider = document.getElementById('squareNumber')
 
-function makeSketch() {
-    sketchPad.style.gridTemplateColumns = `repeat(${SIZE}, 1fr)`
-    sketchPad.style.gridTemplateRows = `repeat(${SIZE}, 1fr)`
-    for (let i = 0; i < SIZE*SIZE; i++) {
+let size = slider.value;
+let color;
+
+//default size displayed under the slider
+sliderValue.textContent = `${slider.value} x ${slider.value}`;
+
+function makeSketch(size) {
+    sketchPad.style.gridTemplateColumns = `repeat(${size}, 1fr)`
+    sketchPad.style.gridTemplateRows = `repeat(${size}, 1fr)`
+    for (let i = 0; i < size * size; i++) {
         const sketchGrid = document.createElement('div');
         sketchGrid.classList.add('grid')
+        sketchGrid.addEventListener('mouseover', setColor)
         sketchPad.appendChild(sketchGrid);
     }
 }
-makeSketch()
 
-const grids = document.querySelectorAll('.grid')
-
-buttons.forEach(button => button.addEventListener('click', (e)=> {
-    if (e.target.value === 'dark') {
-        grids.forEach(grid => grid.addEventListener('mouseover', darken))
-    } else if (e.target.value === 'rainbow') {
-        grids.forEach(grid => grid.addEventListener('mouseover', colorize))
-    }
-}))
-
-function colorize() {
-    this.classList.add('color')
-    this.classList.remove('dark')
-    this.classList.remove('eraser')
+slider.oninput = function() {
+    sliderValue.textContent = `${this.value} x ${this.value}`
+    newSize = this.value;
+    reloadSketch()
 }
 
-function darken() {
-    this.classList.add('dark')
-    this.classList.remove('color')
-    this.classList.remove('eraser')
+function changeSize(newSize) {
+    size = newSize
+}
+
+function reloadSketch() {
+    sketchPad.innerHTML = "" //clearing sketchpad
+    changeSize(newSize)
+    makeSketch(size)
+}
+
+color='blank'
+
+function setColor(e) {
+    if (color === 'dark') {
+        e.target.style.backgroundColor = 'black'
+    } else if (color === 'rainbow') {
+        let rRed = Math.floor(Math.random()*256)
+        let rGreen = Math.floor(Math.random()*256)
+        let rBlue = Math.floor(Math.random()*256)
+        e.target.style.backgroundColor = `rgb(${rRed},${rGreen},${rBlue})`
+    } else if (color === 'blank') {
+        e.target.style.backgroundColor = 'white'
+    }
+}
+
+
+
+
+window.onload = ()=> {
+    makeSketch(size)
 }
